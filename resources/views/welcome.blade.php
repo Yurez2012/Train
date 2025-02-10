0fe24fa0d26269df73910c42aa6cd69abcfaf32d
yuraludchak <yura.ludchak@ralabs.org>
2025-02-10 18:07:58 +0200

    service

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,6 +19,8 @@
             </tr>
         </thead>
         <tbody>
+            @php $count = 0; @endphp
+
             @foreach($result as  $key => $item)
                 <tr>
                     <td class="px-4 whitespace-nowrap border border-gray-300 dark:border-gray-600">
@@ -32,11 +34,28 @@
                     </td>
                     <td class="px-4 whitespace-nowrap border border-gray-300 dark:border-gray-600">
                         @if(isset($item['arrives']['date']) && isset($item['departure']['date']))
-                            {{ Carbon\Carbon::parse($item['arrives']['date'])->diffInMinutes($item['departure']['date']) }}
+                            @php $count +=  Carbon\Carbon::parse($item['arrives']['date'])->diffInMinutes($item['departure']['date']) @endphp
+                            {{ Carbon\Carbon::parse($item['arrives']['date'])->diffInMinutes($item['departure']['date']) }} Minutes
                         @endif
                     </td>
                 </tr>
             @endforeach
+            <tr>
+                <td class="px-4 whitespace-nowrap border border-gray-300 dark:border-gray-600">
+                    Total delay
+                </td>
+                <td class="px-4 whitespace-nowrap border border-gray-300 dark:border-gray-600">
+
+                </td>
+                <td class="px-4 whitespace-nowrap border border-gray-300 dark:border-gray-600">
+
+                </td>
+                <td class="px-4 whitespace-nowrap border border-gray-300 dark:border-gray-600">
+                    {{ $count }} Minutes
+                </td>
+            </tr>
         </tbody>
     </table>
+
+
 @endsection
